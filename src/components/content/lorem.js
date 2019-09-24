2753f8b5598a270e4e92f5a56bfc4642f269d4a2
Juniper Alcorn <juniper@orbitalrx.com>
2019-09-24 16:38:42 -0400

    create paper with two cards

--- src/components/content/lorem.js
+++ src/components/content/lorem.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react'
 
 import { withStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
-import { classes } from 'istanbul-lib-coverage'
+import Paper from '@material-ui/core/Paper'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader' 
+import CardContent from '@material-ui/core/CardContent'
+import { Typography } from '@material-ui/core'
 
 class Lorem extends Component {
   constructor(props){
@@ -13,18 +17,54 @@ class Lorem extends Component {
   render(){
     const {classes} = this.props
     return(
-      <Box className={classes.box}>
-        Cos’è Lorem Ipsum?
-        Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
-      </Box>
+
+      <div >
+        <Paper className={classes.paper}>
+          <Card className={classes.card}>
+            <CardHeader title='Lorem Ipsum' />
+            <CardContent>
+              <Typography>
+                Cos’è Lorem Ipsum?
+                Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
+              </Typography>
+            </CardContent>
+
+          </Card>
+
+          <Card className={classes.card}>
+            <CardHeader title='Lorem Ipsum'>
+            </CardHeader>
+            <CardContent>
+              <Typography>
+                Cos’è Lorem Ipsum?
+                Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
+              </Typography>
+            </CardContent>
+
+          </Card>
+
+        </Paper>
+      </div>
     )
   }
 }
 
 const styles = theme =>({
-  box:{
-    toolbarMargin: theme.mixins.toolbar
+  paper:{
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '2%',
+    margin: '3%',
+    height: '70vh',
+  },
+  card:{
+    // toolbarMargin: theme.mixins.toolbar,
+    width: '50%',
+    margin: '2%',
+  },
+  content:{
+    justifyContent: 'justify',
   }
 })
 
-export default withStyles()(Lorem)
+export default withStyles(styles)(Lorem)
