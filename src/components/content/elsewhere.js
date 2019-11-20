abc2fd6cf5d6f52947ddf63e900a048deb19f515
Juniper Alcorn <juniper@orbitalrx.com>
2019-11-20 17:07:14 -0500

    naive key down solution

--- src/components/content/elsewhere.js
+++ src/components/content/elsewhere.js
@@ -2,15 +2,113 @@ import React, {Component} from 'react'
 
 import { withStyles } from '@material-ui/core/styles'
 
+import Shortcut from '../shortcut'
+
+const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// const shortcuts = {
+//   selectInList(event){
+//     if (event.key !=='h') return false
+//     return true
+//   }
+// }
+
+// let registeredShortcuts;
+
+// const handleKeyDown = event => {  
+//   for (let i = 0; i < registeredShortcuts.length; i += 1) {    
+//     const shortcut = registeredShortcuts[i];    
+//     if (shortcuts[shortcut](event)) {      
+//       event.preventDefault();      
+//       break;    
+//     }  
+//   }
+// };
+
+
+
+// document.addEventListener('keydown', handleKeyDown);
+
+//ctr key = 17
+
+
 class Elsewhere extends Component {
   constructor(props){
     super(props)
-    this.state={}
+    this.state={
+      tool: null,
+      selectedItem: null,
+      selectedList: null
+    }
   }
+
+  componentWillMount(){
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount(){
+
+  }
+
+  handleKeyDown = (e) => {
+    console.log(e)
+    if (e.keyCode === 40) {
+      console.log('in first if block')
+      if(this.state.selectedList === 'select'){
+        console.log('in state list')
+        if (this.state.selectedItem === null){
+          console.log('in state num null')
+          this.setState({selectedItem: 1})
+        } else {
+          console.log('in state +1')
+          let next = this.state.selectedItem + 1
+          this.setState({selectedItem: next})
+        }
+      }
+
+    } else if (e.keyCode === 16 & e.which === 38) {
+
+    } 
+   }
+
+  useTool = (tool) => {
+    if (tool !== this.state.tool) {
+      this.setState({ tool });
+    }
+  }
+  
 
   render(){
     return(
-      <div>Elsewhere</div>
+      <div>
+        <h1>Elsewhere</h1>
+        {list.map(el=>(
+            <div
+            // onClick={this.useTool(select)}
+            id={`${el}-select`}
+            onClick={()=>{
+              this.setState({selectedItem: el, selectedList: 'select'})
+              console.log(this.state.selectedItem)
+            }}
+            style={{backgroundColor: (this.state.selectedItem === el  && this.state.selectedList === `select`)? 'purple' : 'white'}}
+            >
+              {el} item{el > 1 ? 's' : ''} for display and selection.
+            </div>
+        ))}
+
+        <h2>Another List</h2>
+        {list.map(el=>(
+            <div
+            id={`${el}-no-select`}
+            onClick={()=>{
+              this.setState({selectedItem: el, selectedList: 'no-select'})
+            }}
+            style={{backgroundColor: (this.state.selectedItem === el && this.state.selectedList === `no-select`)? 'purple' : 'white'}}
+            >
+              {el} item{el > 1 ? 's' : ''} that better not get selected.
+            </div>
+        ))}
+
+      </div>
     )
   }
 }
---
+++ src/components/shortcut.js
@@ -0,0 +1,30 @@
+// import React from 'react'
+ 
+
+// class Shortcut extends React.Component {
+//   // constructor(props){
+//   //   super(props)
+//   //   this.state={
+//   //     registeredShortcuts: []
+//   //   }
+//   // }
+  
+//   componentDidMount() {    
+//     if (!registeredShortcuts.includes(this.props.name)) {      
+//       registeredShortcuts = [        
+//         ...registeredShortcuts,        
+//         this.props.name,      
+//       ];    
+//     }  
+//   }  
+//   componentWillUnmount() {    
+//     registeredShortcuts = [      
+//       ...registeredShortcuts.filter(x => x !== this.props.name),    
+//     ];  
+//   }  
+//   render() {    
+//     return this.props.children || null;  
+//   }
+// }
+
+// export default Shortcut
